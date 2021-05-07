cb95ad7f1a35a7340eb58cc51799855247e5c499
Mustafa Birhat Gemsiz <birhatgemsiz@hotmail.com>
2021-05-07 22:19:02 +0300

    Bilgi
    ENTITY VE FACADE KULUP OLUSTURULDU

---
+++ ENTITYLAYER/ENTITYNOT.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITYLAYER
+{
+    public class ENTITYNOT
+    {
+        int _OGRENCIID;
+        int _SINAV1;
+        int _SINAV2;
+        int _SINAV3;
+        int _PROJE;
+        double _ORTALAMA;
+        bool _DURUM;
+
+        public int OGRENCIID { get => _OGRENCIID; set => _OGRENCIID = value; }
+        public int SINAV1 { get => _SINAV1; set => _SINAV1 = value; }
+        public int SINAV2 { get => _SINAV2; set => _SINAV2 = value; }
+        public int SINAV3 { get => _SINAV3; set => _SINAV3 = value; }
+        public int PROJE { get => _PROJE; set => _PROJE = value; }
+        public double ORTALAMA { get => _ORTALAMA; set => _ORTALAMA = value; }
+        public bool DURUM { get => _DURUM; set => _DURUM = value; }
+    }
+}
---
+++ FACADELAYER/SQLBAGLANTI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace FACADELAYER
+{
+   public class SQLBAGLANTI
+    {
+      public static SqlConnection baglanti = new SqlConnection(@"Data Source=MUSTAFA;Initial Catalog=Db_Katman;Integrated Security=True");
+
+    }
+}
